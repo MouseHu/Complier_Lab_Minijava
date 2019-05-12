b7a4080b10ab270e17c6ef9f92b77c30cd3ab5b6
MouseHu <540265415@qq.com>
2019-05-12 20:05:33 +0800

    spiglet finally

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,61 @@ import visitor.*;
 //import visitor_piglet;
 //import syntaxtree_piglet.*;
 import syntaxtree.*;
+import syntaxtree_piglet.ParseException;
 import syntaxtree_piglet.PigletParser;
 import symbol.*;
 import piglet.*;
 import spiglet.*;
 public class Main {
+	
+	
+	
+	
+	
 	public static void main(String args[]) throws syntaxtree_piglet.ParseException{
 		try{
-			//InputStream in = new FileInputStream(args[0]);
-			//InputStream in = new FileInputStream("./examples/TreeVisitor.java");// BinaryTree BubbleSort Factorial LinearSearch MoreThan4 LinkedList QuickSort TreeVisitor
-			//InputStream in = new FileInputStream("./examples/TreeVisitor-Error.java"); 1-PrintLiteral 2-Add 3-Call 4-Vars 5-OutOfBounds
+			// BinaryTree BubbleSort Factorial LinearSearch MoreThan4 LinkedList QuickSort TreeVisitor
+			//1-PrintLiteral 2-Add 3-Call 4-Vars 5-OutOfBounds
 			
+			piglet2spiglet("./piglet/examples/QuickSort");
 			
-			String parse_file = "examples/MoreThan4";//input file path  TreeVisitor BubbleSort MoreThan4
-//			InputStream in = new FileInputStream("./"+parse_file+".java");
-			InputStream in = new FileInputStream("./piglet/"+parse_file+".txt");
-			String outfile = "./piglet/"+parse_file+"_spg.txt";//output file path
-			new PigletParser(in);
-//			new MiniJavaParser(in);
-//			Node root = MiniJavaParser.Goal();
-//			
-//			BuildSymbolTableVisitor symbolTableVisitor= new BuildSymbolTableVisitor();
-//			root.accept(symbolTableVisitor,(MType)null);
-//			symbolTableVisitor.InheritCheck();
-//			
-//			HashMap<Pair<String,MType>,MType> symbolTable = symbolTableVisitor.getTable();
-//			root.accept(new TypeCheckVisitor(symbolTable),symbolTableVisitor.getGlobalScope());
-//			//System.out.println(symbolTable);
-//			System.out.println("Type Check Finished. No Error Found.");
-//			root.accept(new TranslateVisitor(symbolTable,outfile),symbolTableVisitor.getGlobalScope());
 
-			
-			syntaxtree_piglet.Node root2 = syntaxtree_piglet.PigletParser.Goal();
-			SpigletVisitor spigletvisitor = new SpigletVisitor(outfile);
-			root2.accept(spigletvisitor,null);
 		} catch (TokenMgrError e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void piglet2spiglet(String arg0) throws FileNotFoundException, ParseException{
+		String parse_file = arg0;//input file path  TreeVisitor BubbleSort MoreThan4
+		InputStream in = new FileInputStream(parse_file+".txt");
+		String outfile = parse_file+".spg";//output file path
+		
+		//Translate
+		new PigletParser(in);
+		syntaxtree_piglet.Node root = syntaxtree_piglet.PigletParser.Goal();
+		root.accept(new SpigletVisitor(outfile),null);
+		System.out.println("Piglet to sPiglet Finished");
+	}
+	
+	public static void minijava2piglet(String arg0) throws ParseException, FileNotFoundException, Exception{
+		String parse_file = arg0;//input file path
+		InputStream in = new FileInputStream("./"+parse_file+".java");
+		String outfile = "./piglet/"+parse_file+".spg";//output file path
+		
+		new MiniJavaParser(in);
+		Node root = MiniJavaParser.Goal();
+		//Build Symbol Table
+		BuildSymbolTableVisitor symbolTableVisitor= new BuildSymbolTableVisitor();
+		root.accept(symbolTableVisitor,(MType)null);
+		symbolTableVisitor.InheritCheck();
+		//Type check
+		HashMap<Pair<String,MType>,MType> symbolTable = symbolTableVisitor.getTable();
+		root.accept(new TypeCheckVisitor(symbolTable),symbolTableVisitor.getGlobalScope());
+		System.out.println("Type Check Finished. No Error Found.");
+		//Translate
+		root.accept(new TranslateVisitor(symbolTable,outfile),symbolTableVisitor.getGlobalScope());
+		System.out.println("MiniJava to Piglet Finished");
 	}
 }
